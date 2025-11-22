edf56a7e43b51e50bfec9a3327e31303fcddd923
Clay Ashby <kingpoiuy@gmail.com>
2025-11-22 08:04:25 -0500

    add gravity

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,7 +1,20 @@
 using UnityEngine;
 
+
+
 public class movement : MonoBehaviour
 {
+    //references
+    public CharacterController controller;
+
+//gravity
+    public float gravity = -9.81f;
+    public LayerMask groundMask;
+    public Transform groundCheck;
+    public Vector3 downwardVelocity;
+    Vector3 jumpVelocity;
+    Vector3 moveDirection;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +24,28 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ApplyGravity(groundCheck, groundMask, controller);
     }
+
+private void ApplyGravity(Transform groundCheck, LayerMask groundMask, CharacterController controller)
+    {
+        if (groundCheck != null)
+        {
+            float gravitysAcceleration = -9.81f;
+            float groundDistance = .4f;
+            bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded)
+            {
+                downwardVelocity.y = -2f;
+            }
+            else
+            {
+                downwardVelocity.y += gravitysAcceleration * Time.deltaTime;
+            }
+
+            controller.Move(downwardVelocity * Time.deltaTime);
+        }
+    }
+
 }
