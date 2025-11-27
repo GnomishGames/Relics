0d7499eb91423b26aed722de4e64cb458233a88c
Clay Ashby <kingpoiuy@gmail.com>
2025-11-27 11:26:14 -0500

    added clipping plane slider

--- Assets/_Custom/Scripts/Interface/Settings/settingsPanel.cs
+++ Assets/_Custom/Scripts/Interface/Settings/settingsPanel.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class settingsPanel : MonoBehaviour
 {
+    public Camera cam;
+    public Slider clippingSlider;
+    public Slider fovSlider;
 
+    void Start()
+    {
+        cam = Camera.main;
+        clippingSlider.value = 100;
+        fovSlider.value = cam.fieldOfView;
+    }
+
+    void Update()
+    {
+        //update clipping plane
+        cam.farClipPlane = clippingSlider.value;
+        //field of view
+        cam.fieldOfView = fovSlider.value;
+    }
 }
