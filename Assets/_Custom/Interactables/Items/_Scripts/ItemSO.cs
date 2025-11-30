5875692f10b8c85b3be97b8a52d24372489786c5
Clay Ashby <kingpoiuy@gmail.com>
2025-11-30 17:42:27 -0500

    Did i change anything?

---
+++ Assets/_Custom/Interactables/Items/_Scripts/ItemSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class ItemSO : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemIcon;
+    public float itemWeight;
+    public int itemHealth;
+}
