27c0f6d2b879c4c2a189e89afd8389efd3b92c24
Clay Ashby <kingpoiuy@gmail.com>
2025-12-04 22:09:48 -0500

    A whole bunch of stuff, mostly from my other project.

--- Assets/_Custom/Interface/Target/TargetPanel.cs
+++ Assets/_Custom/Interface/Target/TargetPanel.cs
@@ -5,22 +5,22 @@ public class TargetPanel : MonoBehaviour
 {
     public TextMeshProUGUI targetNameText;
     [Tooltip("Reference to the Focus script which stores the currently clicked target (playerTarget GameObject). If left empty the script will try to find one in the scene.")]
-    public Focus focusScript;
+    public Focus focus;
 
     void Start()
     {
-        focusScript = GameObject.FindWithTag("Player").GetComponent<Focus>();
+        focus = GameObject.FindWithTag("Player").GetComponent<Focus>();
     }
 
     void Update()
     {
-        if (focusScript.playerTarget == null)
+        if (focus.focus == null)
         {
             targetNameText.text = "No Target";
             return;
         }else{
             //update target name text
-            targetNameText.text = focusScript.playerTarget.name;
+            targetNameText.text = focus.focus.name;
         }
     }
 }
