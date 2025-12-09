71d990dbb569fd0e1d17b0a75a8ae0acbdf3e5e3
Clay Ashby <kingpoiuy@gmail.com>
2025-12-09 07:40:23 -0500

    added basic faction script.

---
+++ Assets/_Custom/Interactables/Characters/_Scripts/Faction.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Faction : MonoBehaviour
+{
+    //this script will calculate and store faction relationships
+    [Range(0, 100)] public int Greenskins;
+    [Range(0, 100)] public int Canis;
+    [Range(0, 100)] public int Rattus;
+    [Range(0, 100)] public int Vampyrus;
+    [Range(0, 100)] public int Humans;
+}
