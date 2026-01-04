8f07000ce09ae4ec4699d0feb36390b645410fb7
Clay Ashby <kingpoiuy@gmail.com>
2026-01-04 15:51:18 -0500

    final commit before changing over to prefabs attached to itemSO

--- Assets/_Custom/Interactables/Items/_Scripts/ItemSO.cs
+++ Assets/_Custom/Interactables/Items/_Scripts/ItemSO.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class ItemSO : ScriptableObject
 {
     public string itemName;
+    public GameObject itemPrefab;
     public Sprite sprite;
     public float itemWeight;
     public int itemHealth;
     public SlotType slotType;
-
 
 }
